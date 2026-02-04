8b37e5d5067af0ea60a853731f5dafe014daa3b3
Alupa87 <13380160779@163.com>
2026-02-04 14:33:20 +0800

    chore:新增.gitignore,配置python项目忽律规则

--- client.py
+++ client.py
@@ -1,19 +1,20 @@
 # 导入内置socket库
 import socket
-from test import Handler
+from server import Handler
 import threading
+
 # 1. 创建 TCP Socket 对象（和服务端一致，AF_INET+SOCK_STREAM）
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# 2. 连接服务端（参数=服务端的IP+端口，必须和服务端bind的一致！）
-server_addr = ('127.0.0.1', 8080)
-client_socket.connect(server_addr)
-print(f"成功连接服务端：{server_addr[0]}:{server_addr[1]}")
-
-# 3. 收发消息（和服务端对应，先发送再接收）
-if __name__=='__init__':
-    client_handler=Handler()
+# 收发消息（和服务端对应，先发送再接收）
+if __name__=='__main__':
+    addr='127.0.0.1'
+    port=8080
     msg=client_socket.recv(1024)
-    t1=threading.Thread(target=Handler.Receive,args=(msg),daemon=True)
-    t2=threading.Thread(target=Handler.Send,daemon=True)
-    
+    client_handler=Handler(addr,port,msg)
+    client_handler.TCP_connect()
+    t1=threading.Thread(target=Handler.Receive,args=(msg),daemon=True).start()
+    t2=threading.Thread(target=Handler.Send,daemon=True).start()
+    while True:
+        if client_handler.Receive() == 'exit':
+            break
--- server.py
+++ server.py
@@ -5,33 +5,40 @@ import time
 # 1. 创建 TCP Socket 对象（AF_INET=IPv4，SOCK_STREAM=TCP协议）
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# 2. 绑定 IP 地址和端口号（元组格式：(IP, 端口)，127.0.0.1=本机，8888=自定义端口）
-server_addr = ('127.0.0.1', 8080)
-server_socket.bind(server_addr)
+class Handler:
+    def __init__(self,addr,port,info):
+        self.addr=addr
+        self.port=port
+        self.info=info
 
-# 3. 监听连接（参数5=最大等待连接数，测试时随便写）
-server_socket.listen(5)
-print(f"服务端已启动，在 {server_addr[0]}:{server_addr[1]} 等待客户端连接...")
+    def TCP_connect(self):
+        server_addr=(self.addr,self.port)
+        server_socket.bind(server_addr)
+        server_socket.listen(5)
+        print(f"端已启动，在 {server_addr[0]}:{server_addr[1]} 等待对方连接...")
+        #接受客户端连接（阻塞式，直到有客户端连过来，返回新的通信Socket+客户端地址）
+        conn, client_addr = server_socket.accept()
+        print(f"成功连接客户端：{client_addr[0]}:{client_addr[1]}")
 
-# 4. 接受客户端连接（阻塞式，直到有客户端连过来，返回新的通信Socket+客户端地址）
-conn, client_addr = server_socket.accept()
-print(f"成功连接客户端：{client_addr[0]}:{client_addr[1]}")
-
-class Handler():
-    def __init__(self,connection):
-        connection=self.connection
-    
-    def Receive(self,info):
-        print(f'Received message:{info.decode('utf-8')}')
-        return info
+    def Receive(self):
+        try:
+            if not isinstance(self.info,bytes):
+                raise f"self.info should be bytes type"
+            print(f"Received message:{self.info.decode('utf-8')}")
+            return self.info.decode('utf-8')
+        except(TypeError,UnicodeDecodeError) as e:
+            return f"Received message failed{str(e)}"
 
     def Send(self):
         msg=input('Send:')
-        msg.encode('utf-8')
+        return msg.encode('utf-8')
 
-if __name__ =='__init__':
-    connect=Handler()
+if __name__ =='__main__':
+    addr='127.0.0.1'
+    port=8080
     msg=server_socket.recv(1024)
+    connect=Handler(addr,port,msg)
+    connect.TCP_connect()
     t1=threading.Thread(target=connect.Send,args=(msg),daemon=True).start()
     t2=threading.Thread(target=connect.Receive,daemon=True).start()
     while True:
