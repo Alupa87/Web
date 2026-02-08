a0c555aa8cd3769838a25fcde18579f01d38b03f
Alupa87 <13380160779@163.com>
2026-02-08 13:54:14 +0800

    初步提交

--- client.py
+++ client.py
@@ -3,18 +3,20 @@ import socket
 from server import Handler
 import threading
 
-# 1. 创建 TCP Socket 对象（和服务端一致，AF_INET+SOCK_STREAM）
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 # 收发消息（和服务端对应，先发送再接收）
 if __name__=='__main__':
+    # 1. 创建 TCP Socket 对象（和服务端一致，AF_INET+SOCK_STREAM）
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     addr='127.0.0.1'
     port=8080
+    client_addr=(addr,port)
+    client_socket.bind(client_addr)
+    server,con=client_socket.connect()
     msg=client_socket.recv(1024)
-    client_handler=Handler(addr,port,msg)
-    client_handler.TCP_connect()
+    client_handler=Handler(msg)
     t1=threading.Thread(target=Handler.Receive,args=(msg),daemon=True).start()
     t2=threading.Thread(target=Handler.Send,daemon=True).start()
     while True:
         if client_handler.Receive() == 'exit':
             break
+    client_socket.close()
--- server.py
+++ server.py
@@ -2,23 +2,9 @@ import socket
 import threading
 import time
 
-# 1. 创建 TCP Socket 对象（AF_INET=IPv4，SOCK_STREAM=TCP协议）
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 class Handler:
-    def __init__(self,addr,port,info):
-        self.addr=addr
-        self.port=port
+    def __init__(self,info):
         self.info=info
-
-    def TCP_connect(self):
-        server_addr=(self.addr,self.port)
-        server_socket.bind(server_addr)
-        server_socket.listen(5)
-        print(f"端已启动，在 {server_addr[0]}:{server_addr[1]} 等待对方连接...")
-        #接受客户端连接（阻塞式，直到有客户端连过来，返回新的通信Socket+客户端地址）
-        conn, client_addr = server_socket.accept()
-        print(f"成功连接客户端：{client_addr[0]}:{client_addr[1]}")
 
     def Receive(self):
         try:
@@ -34,12 +20,21 @@ class Handler:
         return msg.encode('utf-8')
 
 if __name__ =='__main__':
+    # 1. 创建 TCP Socket 对象（AF_INET=IPv4，SOCK_STREAM=TCP协议）
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     addr='127.0.0.1'
     port=8080
-    msg=server_socket.recv(1024)
-    connect=Handler(addr,port,msg)
-    connect.TCP_connect()
-    t1=threading.Thread(target=connect.Send,args=(msg),daemon=True).start()
+    server_addr=(addr,port)
+    server_socket.bind(server_addr)
+    server_socket.listen(5)
+    print(f"端已启动，在 {server_addr[0]}:{server_addr[1]} 等待对方连接...")
+    #接受客户端连接（阻塞式，直到有客户端连过来，返回新的通信Socket+客户端地址）
+    conn, client_addr = server_socket.accept()
+    print(f"成功连接客户端：{client_addr[0]}:{client_addr[1]}")
+
+    info=server_socket.recv(1024)
+    connect=Handler(info)
+    t1=threading.Thread(target=connect.Send,args=(info),daemon=True).start()
     t2=threading.Thread(target=connect.Receive,daemon=True).start()
     while True:
         if connect.Receive() == 'exit':
